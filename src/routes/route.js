54370fef6a4bc1ad1cbf5f10c1e708862bc510c3
supriyahatele <niyahatele70@gmail.com>
2022-06-04 11:32:29 +0530

    post_api_assign

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,4 +70,66 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+router.post('/players', function (req, res) {
+    //LOGIC WILL COME HERE
+
+let players =
+  [
+    {
+      "name": "manish",
+      "dob": "1/1/1995",
+      "gender": "male",
+      "city": "jalandhar",
+      "sports": [
+        "swimming"
+      ]
+    },
+    {
+      "name": "gopal",
+      "dob": "1/09/1995",
+      "gender": "male",
+      "city": "delhi",
+      "sports": [
+        "soccer"
+      ]
+    },
+    {
+      "name": "lokesh",
+      "dob": "1/1/1990",
+      "gender": "male",
+      "city": "mumbai",
+      "sports": [
+        "soccer"
+      ]
+    },
+  ]
+
+let newPlayer = req.body.player
+players.push(newPlayer)
+
+
+let newPlayersName = newPlayer.name
+let isNameRepeated = false
+
+//let player = players.find(p => p.name == newPlayersName)
+
+for(let i = 0; i < players.length; i++) {
+    if(players[i].name == newPlayersName) {
+        isNameRepeated = true;
+        break;
+    }
+}
+
+//undefined is same as false/ a falsy value
+if (isNameRepeated) {
+    //Player exists
+    res.send("This player was already added!")
+} else {
+    //New entry
+    players.push(newPlayer)
+    res.send(players)
+}
+
+})
+
 module.exports = router;
